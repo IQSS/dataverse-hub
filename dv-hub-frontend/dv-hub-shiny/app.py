034581f5ece5f8c470a82bb5547f809a252f8035
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2024-10-18 22:09:06 -0400

    Working map with installations info

--- dv-hub-frontend/dv-hub-shiny/app.py
+++ dv-hub-frontend/dv-hub-shiny/app.py
@@ -1,28 +1,57 @@
 import requests
 import pandas
-from ipyleaflet import Map, Marker, Icon
-
-from shiny import render, ui
-from shiny.express import input
+from shiny.express import input, ui, render
 from shinywidgets import render_widget  
+from ipyleaflet import Map, Marker, AwesomeIcon
 
-ui.panel_title("Dataverse Hub")
+with ui.sidebar():
+    ui.input_checkbox("show_ok", "Show OK", True)
+    ui.input_checkbox("show_unreachable", "Show Unreachable", True)
+    ui.input_checkbox("show_unrecognizable", "Show Unrecognizable", True)
 
-url = "http://localhost:8080/api/installation/status"
-response = requests.get(url)
-df = pandas.json_normalize(response.json())
+ui.page_opts(title="Dataverse Hub")
+ui.page_opts(fillable=True)
 
-@render.data_frame
-def txt():        
-    return render.DataTable(df.drop(columns=['recordId', 'captureDate', 'build']))
+# ui.panel_title("DV Installations")
 
-@render_widget  
-def map():
-    m = Map(center=(50.6252978589571, 0.34580993652344), zoom=3)
-    marker1 = Marker(location=(51.5, -0.09), icon=Icon(color='blue'))
-    marker2 = Marker(location=(51.51, -0.1), icon=Icon(color='green'))
-    marker3 = Marker(location=(51.49, -0.08), icon=Icon(color='red'))
-    m.add_layer(marker1)
-    m.add_layer(marker2)
-    m.add_layer(marker3)
-    return m
+status_url = "http://localhost:8080/api/installation/status"
+status_response = requests.get(status_url)
+status_data_frame = pandas.json_normalize(status_response.json())
+
+installations_url = "http://localhost:8080/api/installation"
+installations_response = requests.get(installations_url)
+installations_data_frame = pandas.json_normalize(installations_response.json())
+
+with ui.layout_columns(fill=True):
+    with ui.card(full_screen=True):
+        ui.card_header("Installations data")
+        @render.data_frame
+        def status_data_chart():        
+            return render.DataTable(status_data_frame.drop(columns=['recordId', 'captureDate', 'build']), width='100%', height='100%', filters=True)
+    
+    with ui.card(full_screen=True):
+        ui.card_header("Interactive Map")
+        @render_widget  
+        def map():
+            m = Map(center=(50.6252978589571, 0.34580993652344), zoom=2)
+            for idx, row in installations_data_frame.iterrows():
+                status = status_data_frame.loc[status_data_frame['dvHubId'] == row['dvHubId'], 'status'].values[0]
+                if status == 'OK':
+                    marker_color = 'lightgreen'
+                    icon_name = 'check'
+                    icon_color = 'lightgreen'
+                elif status == 'UNREACHABLE':
+                    marker_color = 'lightred'
+                    icon_name = 'power-off'
+                    icon_color = 'white'
+                elif status == 'UNRECOGNIZABLE':
+                    marker_color = 'orange'
+                    icon_name = 'warning'
+                    icon_color = 'yellow'
+                else:
+                    color = 'blue'  # Default color if status is unknown
+                marker = Marker(location=(row['latitude'], row['longitude']), icon=AwesomeIcon(name=icon_name, marker_color=marker_color, icon_color=icon_color))                
+                m.add_layer(marker)
+            return m
+
+    
