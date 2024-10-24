855faca61a3d132013d8baa09ad1bd0848fd17a6
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2024-10-24 18:53:40 -0400

    Import -

--- src/main/java/edu/harvard/iq/dataverse_hub/security/SecurityConfig.java
+++ src/main/java/edu/harvard/iq/dataverse_hub/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package edu.harvard.iq.dataverse_hub.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
 
 
