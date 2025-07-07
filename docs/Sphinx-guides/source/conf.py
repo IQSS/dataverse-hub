1112c0030688f081d4005734cb94579cbcb56a7b
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2025-07-07 13:50:36 -0400

    Added Sphinx to create a guide for DV-Hub

---
+++ docs/Sphinx-guides/source/conf.py
@@ -0,0 +1,26 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Dataverse-Hub"
+copyright = "2025, IQSS"
+author = "IQSS"
+release = "beta"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser", "sphinx_design"]
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_logo = "_static/logo.png"
+html_static_path = ["_static"]
--- makefile
+++ makefile
@@ -1,8 +1,14 @@
-make run:
+# Makefile for Dataverse Hub
+PWD := $(shell pwd)
+
+run:
 	mvn -Pct clean package -DskipTests=true docker:build docker:run
 
-make build:
+build:
 	mvn -Pct clean package -DskipTests=true docker:build docker:build
 
-make boot:
+boot:
 	mvn spring-boot:run
+
+guides:
+	docker run -it --rm -v $(PWD):/docs sphinxdoc/sphinx:7.3.6 bash -c "cd docs/sphinx-guides && pip3 install -r requirements.txt && make clean && make html"
